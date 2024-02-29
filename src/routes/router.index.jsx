72091fb34bdf0ef7c493f11a3b4c5d6026135431
AaronGS-Dev <ravycrow@gmail.com>
2024-02-29 09:56:51 +0000

    Login en su carpeta

--- src/routes/router.index.jsx
+++ src/routes/router.index.jsx
@@ -1,11 +1,15 @@
 import {createBrowserRouter} from 'react-router-dom'
 
 import Home from '../pages/home/home'
-
+import Login from '../pages/login/login'
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home />
+    },
+    {
+        path: '/login',
+        element: <Login/>
     }
 ])
 
